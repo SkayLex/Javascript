2481f63b9fe1aed287906d69de1c6d289c41d234
SkayLex <36420988+SkayLex@users.noreply.github.com>
2018-07-11 19:32:23 +0300

    Add files via upload

---
+++ javascript2curs/dom.js
@@ -0,0 +1,16 @@
+//PAGE(html) --> BROWSER
+            // --> PARSE
+            // --> ERRORS
+            // --> BUILDS in MEMORY
+            // --> RENDER --> USER :)
+
+
+
+
+// DOM + BOM
+// Document Object Model
+//Browser Object Model
+// важные элементы дока.....document.head, document.body, document.title;
+// document.images
+// Element -->button,images etc.
+// parentElement , previousElementSibling , nextElementSibling , firstElementChild , lastElementChild;
