a87e378965147166e68b4fd3d8f40f279c11c396
SkayLex <36420988+SkayLex@users.noreply.github.com>
2018-07-18 19:33:43 +0300

    Add files via upload

--- angryBirds_dom/js/bird.js
+++ angryBirds_dom/js/bird.js
@@ -1,17 +1,34 @@
 //COMPONENTS
 class Bird{
    constructor(){
-      this.top = Math.round( 500 * Math.random() );
-      this.left = Math.round( 500 * Math.random() );;
+      this.top = Math.round( ( 500 - 48 )* Math.random() );
+      this.left = Math.round( ( 500 - 48 ) * Math.random() );
       this.color = 'red';
+      this.speed = Math.round(Math.random()*4 + 1);
    }
    //выводит птицу в хтмл
    render(){
-     this.div =document.createElement('div');
+     this.div = document.createElement('div');
      this.div.setAttribute('class' , `bird ${this.color}`);
      document.body.firstElementChild.appendChild( this.div );
      // при помощи position. left , top чтоб птица двигалась
+
+      this.div.style.top = `${this.top}px`;
+      this.div.style.left = `${this.left}px`;
    }
+  fly(){
+    var self = this;
+     setInterval(function(){
+       if(self.left>(250 + Math.random()*200) || self.left<Math.random()*200){
+         self.speed*=-1;
+         if(Math.abs(self.speed) <20){
+           self.speed *= 1.5;
+         }
+       }
+       self.left+=self.speed;
+       self.div.style.left = `${self.left}px`;
+       console.log(self.left);
+     },500);
+ }
+
 }
-var b = new Bird();
-b.render();
---
+++ angryBirds_dom/js/game.js
@@ -0,0 +1,11 @@
+var b = new Bird();
+b.render();
+b.div.onclick = shoot;
+
+// VISUAL<>MEMORY
+function shoot(){
+alert();
+//1) find the bird
+//2)setTimeout(function(){}) ->.removeChild()
+}
+b.fly();
